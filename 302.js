59e318a60ab80cc3be760aedd2f3d8bf0857b906
wangchuanchuan <devilfrank_isy@163.com>
2025-08-07 17:21:48 +0800

    fix()

--- 302.js
+++ 302.js
@@ -1,5 +1,6 @@
 window.onload = function () {
-	window.location.href = 'https://sw519p.qxtzhtvzmpqdb.com/?network=facebook&site=direct&adtitle=Home%20Renovation%20Near%20Me&dpco=1'
+	window.location.href =
+		'https://takemetown.asia/cf/r/67cbdc98627a7400123e25d3?ad_id={{ad.id}}&adset_id={{adset.id}}&campaign_id={{campaign.id}}&ad_name={{ad.name}}&adset_name={{adset.name}}&campaign_name={{campaign.name}}&source={{site_source_name}}&placement={{placement}}'
 }
 
 // https://m.facebook.com
