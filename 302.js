628c49286f70993f79fe549657f280516d62df03
wangchuanchuan <devilfrank_isy@163.com>
2025-08-15 11:53:58 +0800

    fix

--- 302.js
+++ 302.js
@@ -1,6 +1,7 @@
 window.onload = function () {
 	window.location.href =
-		'https://takemetown.asia/cf/r/67cbdc98627a7400123e25d3?ad_id={{ad.id}}&adset_id={{adset.id}}&campaign_id={{campaign.id}}&ad_name={{ad.name}}&adset_name={{adset.name}}&campaign_name={{campaign.name}}&source={{site_source_name}}&placement={{placement}}'
+		'https://webeasyhit.com/cf/r/689c38cf417d3800128a16b2?ad_id=120231466403140019&adset_id=120231466403130019&campaign_id=120231466403120019&ad_name=loan&adset_name=0813&campaign_name=fastloan28.online_DE_Ads_0813&source={{site_source_name}}&placement={{placement}}'
 }
 
 // https://m.facebook.com
+// https://www.tiktok.com/
